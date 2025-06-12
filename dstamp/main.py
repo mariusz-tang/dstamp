f124ad0832c8f99d28103ce3d727e82148531929
Mariusz Tang <dev@mariusztang.com>
2025-06-12 21:37:40 +0100

    add option to copy output to clipboard

--- dstamp/main.py
+++ dstamp/main.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 
+import clipman
 import typer
 from typing_extensions import Annotated
 
@@ -17,9 +18,22 @@ def main(
             help="The format in which the timestamp will be displayed in Discord."
         ),
     ] = format.Format.RELATIVE,
+    copy_to_clipboard: Annotated[
+        bool,
+        typer.Option(
+            "--copy-to-clipboard/--no-copy",
+            "-x",
+            help="If set, copy the timestamp to clipboard. "
+            "On Linux, requires that xsel or xclip be installed.",
+        ),
+    ] = False,
 ):
     output = format.convert_to_discord_format(time, output_format)
     print(output)
+    if copy_to_clipboard:
+        clipman.init()
+        clipman.set(output)
+        print("Copied to clipboard!")
 
 
 if __name__ == "__main__":
