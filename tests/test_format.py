8c33ae97a1545592d007785d254cc6001be84758
Mariusz Tang <dev@mariusztang.com>
2025-06-15 19:28:29 +0100

    rename test for specificity
    This test tests only the convert_to_discord_format function, not the
    entire format test as suggested previously.

--- tests/test_format.py
+++ tests/test_format.py
@@ -7,7 +7,7 @@ from tests.timestamp import Timestamp
 
 
 @pytest.mark.parametrize("form", (form for form in format.Format))
-def test_format(form: format.Format):
+def test_convert_to_discord_format(form: format.Format):
     dt = datetime.datetime.now()
     formatted_time = format.convert_to_discord_format(dt, form)
     stamp = Timestamp(formatted_time)
