7dd6d44be54f14cb31116b454f70e5b52795fd91
Mariusz Tang <dev@mariusztang.com>
2025-07-06 21:26:58 +0100

    add empty config fixture
    This fixture ensures that tests are not affected by any config files on
    the test machine.

---
+++ tests/conftest.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+
+import pytest
+
+from dstamp import config
+
+
+@pytest.fixture(autouse=True)
+def empty_default_config(monkeypatch):
+    """Change the default config location so that it is empty for tests."""
+    test_dir = Path(__file__).resolve().parent
+    empty_config_path = test_dir / "dstamp_config" / "empty.toml"
+    monkeypatch.setattr(config, "get_config_path", lambda: empty_config_path)
