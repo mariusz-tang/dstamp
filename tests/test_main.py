b8417631b63e408e30b68ffa91d90188ab344aca
Mariusz Tang <dev@mariusztang.com>
2025-07-08 17:44:56 +0100

    move main test output parsing to own function
    The added get_output_for_command allows us to write commands as a single
    string and saves on the repetitive runner invokation code.

--- tests/test_main.py
+++ tests/test_main.py
@@ -15,10 +15,21 @@ from tests.utils.patched_time import now
 runner = CliRunner()
 
 
+def get_output_for_command(command: str) -> DstampOutput:
+    """
+    Run the command and parse the output.
+
+    The command must not contain spaces in any arguments (even in quoted
+    strings).
+    """
+    args = command.split(" ")
+    result = runner.invoke(app, args)
+    return DstampOutput(result.output)
+
+
 @freeze_time(now)
 def test_get():
-    result = runner.invoke(app, ["get"])
-    output = DstampOutput(result.output)
+    output = get_output_for_command("get")
     assert output.timestamp.timestamp == int(now.timestamp())
     assert output.timestamp.format_code == Format.RELATIVE.code
     assert not output.copied_to_clipboard
