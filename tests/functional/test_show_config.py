dbb2b195e090984bef3a788847fd092677d060ce
Mariusz Tang <dev@mariusztang.com>
2025-07-11 13:55:43 +0100

    add more show-config tests

--- tests/functional/test_show_config.py
+++ tests/functional/test_show_config.py
@@ -4,8 +4,7 @@ This module contains functional tests for the dstamp show-config command.
 """
 
 from dstamp import format
-from tests.utils import dstamp_cli
-from tests.utils.config import INVALID_TOML_PATH
+from tests.utils import config, dstamp_cli
 
 
 def test_no_parameters(empty_default_config):
@@ -17,8 +16,20 @@ def test_no_parameters(empty_default_config):
 
 
 def test_invalid_toml():
-    output = dstamp_cli.run_show_config(str(INVALID_TOML_PATH))
-    assert output.config_path == INVALID_TOML_PATH
+    output = dstamp_cli.run_show_config(str(config.INVALID_TOML_PATH))
+    assert output.config_path == config.INVALID_TOML_PATH
     assert output.has_using_default_warning
     assert output.invalid_toml
     assert not output.not_a_file
+
+
+def test_copy_config():
+    output = dstamp_cli.run_show_config(str(config.COPY_CONFIG_PATH))
+    assert output.config_path == config.COPY_CONFIG_PATH
+    assert output.copy_to_clipboard is True
+
+
+def test_format_config():
+    output = dstamp_cli.run_show_config(str(config.SHORT_TIME_FORMAT_CONFIG_PATH))
+    assert output.config_path == config.SHORT_TIME_FORMAT_CONFIG_PATH
+    assert output.output_format == format.Format.SHORT_TIME.value
