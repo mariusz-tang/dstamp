3875ff8410118aa3830c04272b91ebd19df056ff
Mariusz Tang <dev@mariusztang.com>
2025-07-15 12:34:41 +0100

    add non-file show-config test

--- tests/functional/test_show_config.py
+++ tests/functional/test_show_config.py
@@ -3,6 +3,9 @@
 This module contains functional tests for the dstamp show-config command.
 """
 
+from pathlib import Path
+
+from dstamp import config as app_config
 from dstamp import format
 from tests.utils import config, dstamp_cli
 
@@ -21,6 +24,18 @@ def test_invalid_toml():
     assert output.has_using_default_warning
     assert output.invalid_toml
     assert not output.not_a_file
+
+
+def test_not_a_file(monkeypatch):
+    # Always use the following directory as the config path.
+    path = Path(__file__).resolve().parent
+    get = app_config.get
+    monkeypatch.setattr(app_config, "get", lambda _: get(path))
+
+    output = dstamp_cli.run_show_config()
+    assert output.has_using_default_warning
+    assert not output.invalid_toml
+    assert output.not_a_file
 
 
 def test_copy_config():
