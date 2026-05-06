a568959db3d1a7064e474e82488ad6c15c2c906c
Mariusz Tang <dev@mariusztang.com>
2026-05-07 00:04:31 +0100

    tests: simplify format test
    Prefer directly testing the final value rather than opaquely feeding it
    through a parser. Also prefer testing set times rather than taking the
    time of test invocation.

--- tests/unit/test_format.py
+++ tests/unit/test_format.py
@@ -1,17 +1,24 @@
 """Tests for dstamp.format."""
 
-import datetime
+from datetime import datetime
 
 import pytest
 
-from dstamp import format
-from tests.utils.parse import Timestamp
+from dstamp.format import Format, convert_to_discord_format
 
 
-@pytest.mark.parametrize("form", (form for form in format.Format))
-def test_convert_to_discord_format(form: format.Format):
-    dt = datetime.datetime.now()
-    formatted_time = format.convert_to_discord_format(dt, form)
-    stamp = Timestamp(formatted_time)
-    assert stamp.format_code == form.value
-    assert stamp.timestamp == int(dt.timestamp())
+@pytest.mark.parametrize(
+    "time,format,expected_result",
+    [
+        (datetime(2026, 5, 6, 23, 54, 7), Format.LONG_DATE, "<t:1778108047:D>"),
+        (datetime(2025, 5, 6, 23, 54, 7), Format.LONG_DATETIME, "<t:1746572047:F>"),
+        (datetime(2026, 4, 6, 23, 54, 7), Format.LONG_TIME, "<t:1775516047:T>"),
+        (datetime(2026, 5, 3, 23, 54, 7), Format.RELATIVE, "<t:1777848847:R>"),
+        (datetime(2026, 5, 6, 13, 54, 7), Format.SHORT_DATE, "<t:1778072047:d>"),
+        (datetime(2026, 5, 6, 23, 24, 7), Format.SHORT_DATETIME, "<t:1778106247:f>"),
+        (datetime(2032, 5, 6, 23, 54, 7), Format.SHORT_TIME, "<t:1967496847:t>"),
+    ],
+)
+def test_convert_to_discord_format(time, format, expected_result):
+    formatted_time = convert_to_discord_format(time, format)
+    assert formatted_time == expected_result
