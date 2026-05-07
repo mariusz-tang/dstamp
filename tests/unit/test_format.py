90ed96d3541bb9fb5a670728393e30c88767ec74
Mariusz Tang <dev@mariusztang.com>
2026-05-07 12:03:27 +0100

    tests: fix format tests
    - Use timezone-aware datetime objects so the results are consistent
      between machines.
    - Construct dates from directly from timestamps since that is ultimately
      what we are testing and it's cleaner to write this way.

--- tests/unit/test_format.py
+++ tests/unit/test_format.py
@@ -1,6 +1,6 @@
 """Tests for dstamp.format."""
 
-from datetime import datetime
+from datetime import UTC, datetime
 
 import pytest
 
@@ -8,17 +8,18 @@ from dstamp.format import Format, convert_to_discord_format
 
 
 @pytest.mark.parametrize(
-    "time,format,expected_result",
+    "timestamp,format,expected_result",
     [
-        (datetime(2026, 5, 6, 23, 54, 7), Format.LONG_DATE, "<t:1778108047:D>"),
-        (datetime(2025, 5, 6, 23, 54, 7), Format.LONG_DATETIME, "<t:1746572047:F>"),
-        (datetime(2026, 4, 6, 23, 54, 7), Format.LONG_TIME, "<t:1775516047:T>"),
-        (datetime(2026, 5, 3, 23, 54, 7), Format.RELATIVE, "<t:1777848847:R>"),
-        (datetime(2026, 5, 6, 13, 54, 7), Format.SHORT_DATE, "<t:1778072047:d>"),
-        (datetime(2026, 5, 6, 23, 24, 7), Format.SHORT_DATETIME, "<t:1778106247:f>"),
-        (datetime(2032, 5, 6, 23, 54, 7), Format.SHORT_TIME, "<t:1967496847:t>"),
+        (0, Format.LONG_DATE, "<t:0:D>"),
+        (1725062400, Format.LONG_DATETIME, "<t:1725062400:F>"),
+        (93032, Format.LONG_TIME, "<t:93032:T>"),
+        (2923, Format.RELATIVE, "<t:2923:R>"),
+        (392, Format.SHORT_DATE, "<t:392:d>"),
+        (-500, Format.SHORT_DATETIME, "<t:-500:f>"),
+        (39042343, Format.SHORT_TIME, "<t:39042343:t>"),
     ],
 )
-def test_convert_to_discord_format(time, format, expected_result):
+def test_convert_to_discord_format(timestamp, format, expected_result):
+    time = datetime.fromtimestamp(timestamp, tz=UTC)
     formatted_time = convert_to_discord_format(time, format)
     assert formatted_time == expected_result
