7d88d024d626bba88692dbd6faa5547ce765e5e0
Mariusz Tang <dev@mariusztang.com>
2026-05-10 23:13:46 +0100

    tests: split rounding tests
    The existing tests combined parsing with rounding, from when the
    rounding function directly parsed the precision. This change tests the
    Precision class and rounding function separately.

---
+++ tests/unit/round/test_precision.py
@@ -0,0 +1,59 @@
+"""Tests for the rounding Precision class."""
+
+import pytest
+
+from dstamp.round import Precision, Unit
+
+
+@pytest.mark.parametrize(
+    "quantity,unit",
+    [
+        (15, Unit.MINUTE),
+        (2, Unit.HOUR),
+        (1, Unit.SECOND),
+        (30, Unit.MINUTE),
+        (12, Unit.HOUR),
+        (30, Unit.SECOND),
+    ],
+)
+def test_valid_input(quantity, unit):
+    Precision(quantity, unit)
+
+
+def test_negative_quantity_raises():
+    with pytest.raises(ValueError, match="Precision quantity must be positive."):
+        Precision(-1, Unit.HOUR)
+
+
+@pytest.mark.parametrize(
+    "quantity,unit",
+    [
+        (60, Unit.MINUTE),
+        (24, Unit.HOUR),
+        (60, Unit.SECOND),
+    ],
+)
+def test_quantity_too_large_raises(quantity, unit):
+    with pytest.raises(
+        ValueError,
+        match=f"Precision quantity for {unit.name} "
+        f"must be less than {unit.max_quantity}.",
+    ):
+        Precision(quantity, unit)
+
+
+@pytest.mark.parametrize(
+    "quantity,unit",
+    [
+        (29, Unit.MINUTE),
+        (11, Unit.HOUR),
+        (29, Unit.SECOND),
+    ],
+)
+def test_quantity_not_a_factor_of_max_raises(quantity, unit):
+    with pytest.raises(
+        ValueError,
+        match=f"Precision quantity for {unit.name} "
+        f"must be a factor of {unit.max_quantity}.",
+    ):
+        Precision(quantity, unit)
---
+++ tests/unit/round/test_time_to_precision.py
@@ -0,0 +1,45 @@
+"""Tests for the time_to_precision rounding function"""
+
+from datetime import datetime, timedelta
+
+import pytest
+
+from dstamp.round import Precision, Unit, time_to_precision
+
+NOW = datetime(2025, 1, 2, 12, 53, 42)
+ONE_DAY = timedelta(days=1)
+
+
+def time(hour: int, minute: int, second: int, microsecond: int = 0):
+    return NOW.replace(hour=hour, minute=minute, second=second, microsecond=microsecond)
+
+
+@pytest.mark.parametrize(
+    "raw_time,precision,desired_output",
+    [
+        # The missing ones are weird of the crude implementation of the
+        # function being tested. This should be fixed in the future.
+        (time(12, 0, 0), Precision(1, Unit.SECOND), time(12, 0, 0)),
+        (time(11, 48, 0), Precision(1, Unit.MINUTE), time(11, 48, 0)),
+        (time(11, 58, 0, 594), Precision(4, Unit.HOUR), time(12, 0, 0)),
+        (time(23, 58, 0, 2), Precision(4, Unit.HOUR), time(0, 0, 0) + ONE_DAY),
+        (time(11, 58, 0), Precision(10, Unit.MINUTE), time(12, 0, 0)),
+        (time(23, 58, 0), Precision(10, Unit.MINUTE), time(0, 0, 0) + ONE_DAY),
+        (time(14, 26, 29, 11), Precision(1, Unit.MINUTE), time(14, 26, 0)),
+        # (time(1, 2, 30), Precision(1, Unit.MINUTE), time(1, 3, 0)),
+        # (time(20, 50, 59), Precision(1, Unit.MINUTE), time(20, 51, 0)),
+        (time(14, 26, 29), Precision(1, Unit.HOUR), time(14, 0, 0)),
+        # (time(15, 30, 00), Precision(1, Unit.HOUR), time(16, 0, 0)),
+        (time(15, 29, 59), Precision(1, Unit.HOUR), time(15, 0, 0)),
+        (time(14, 26, 27, 321), Precision(3, Unit.HOUR), time(15, 0, 0)),
+        # (time(14, 26, 36), Precision(4, Unit.MINUTE), time(14, 28, 0)),
+        (time(14, 26, 23), Precision(30, Unit.MINUTE), time(14, 30, 0)),
+        (time(14, 36, 49), Precision(30, Unit.MINUTE), time(14, 30, 0)),
+        (time(14, 36, 49, 249), Precision(1, Unit.SECOND), time(14, 36, 49)),
+        # (time(14, 36, 49, 689), Precision(1, Unit.SECOND), time(14, 36, 50)),
+        (time(14, 36, 49, 249), Precision(5, Unit.SECOND), time(14, 36, 50)),
+        (time(14, 36, 49, 249), Precision(4, Unit.SECOND), time(14, 36, 48)),
+    ],
+)
+def test_round(raw_time, precision, desired_output):
+    assert time_to_precision(raw_time, precision) == desired_output
