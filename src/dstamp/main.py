e52ff39a43df07527b0bf00b73b47f53e272341d
Mariusz Tang <dev@mariusztang.com>
2026-05-13 11:43:07 +0100

    fix: help message format
    Note that the help message is parsed as markdown. We need to escape the
    opening angle bracket in `<date>` and `<time>` so they are not
    interpretted as tags. In the future, I intend to rewrite the help
    message to not use this format.

--- src/dstamp/main.py
+++ src/dstamp/main.py
@@ -49,33 +49,33 @@ def get_timestamp(
 
     If TIME is omitted, uses the current time.
 
-    TIME should be of the form <date>,<time>.
-    Either <date> or <time> can be omitted, in which case you should omit the
+    TIME should be of the form \\<date>,\\<time>.
+    Either \\<date> or \\<time> can be omitted, in which case you should omit the
     comma as well.
 
-    <date> should be of the following form: ddmmmyyyy.
+    \\<date> should be of the following form: ddmmmyyyy.
 
     dd and yyyy should be numbers.
 
     mmm should be at least the first three letters of the month. For instance,
     for August you can specify aug, augu, augus, or august.
 
-    You can omit the year to specify the current year. You can omit <date>
+    You can omit the year to specify the current year. You can omit \\<date>
     entirely to specify the current date.
 
     The command will also accept the following special values:
-    today,
-    tmrw (tomorrow),
-    tomorrow,
-    yesterday.
+    - today
+    - tmrw (tomorrow)
+    - tomorrow
+    - yesterday
 
-    <time> should be of the following form: hhmmss(am/pm).
+    \\<time> should be of the following form: hhmmss(am/pm).
 
     You can omit seconds to specify 0. If you do, you can omit minutes to specify
-    0 as well. If you omit (am/pm), 24-hour time will be used. You can omit <time>
+    0 as well. If you omit (am/pm), 24-hour time will be used. You can omit \\<time>
     entirely to specify midnight.
 
-    If you omit both <date> and <time>, the current date and time will be used.
+    If you omit both \\<date> and \\<time>, the current date and time will be used.
 
     The command will also accept the following special values:
     now (the current time, but not necessarily the current date),
@@ -83,15 +83,15 @@ def get_timestamp(
     noon.
 
     Examples:
-    5jun,1pm (5th June of the current year, 1:00pm),
-    8january2025,1530 (8th January 2025, 3:30pm),
-    25nov2000,13002pm (25th November 2000, 1:30:02pm),
-    1jan (1st January of the current year, midnight),
-    7feb2023,15 (7th February 2023, 3:00pm),
-    5 (the current date, 5:00am),
-    0 (the current date, midnight),
-    tmrw,now (tomorrow, the current time, ie. 24 hours from the current time),
-    tmrw (tomorrow, midnight).
+    - 5jun,1pm (5th June of the current year, 1:00pm)
+    - 8january2025,1530 (8th January 2025, 3:30pm)
+    - 25nov2000,13002pm (25th November 2000, 1:30:02pm)
+    - 1jan (1st January of the current year, midnight)
+    - 7feb2023,15 (7th February 2023, 3:00pm)
+    - 5 (the current date, 5:00am)
+    - 0 (the current date, midnight)
+    - tmrw,now (tomorrow, the current time, ie. 24 hours from the current time)
+    - tmrw (tomorrow, midnight)
 
     :param time: The date and time to which the timestamp should point. If
     omitted, the current time is used.
