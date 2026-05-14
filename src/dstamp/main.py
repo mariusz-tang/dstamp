5bd5f56642b26d8b101817862d6b01c0d99dca32
Mariusz Tang <dev@mariusztang.com>
2026-05-14 23:50:26 +0100

    refactor: use raw docstring instead of escapes

--- src/dstamp/main.py
+++ src/dstamp/main.py
@@ -44,23 +44,23 @@ def get_timestamp(
     round: bool = False,
     precision: str = "10m",
 ) -> int:
-    """
+    r"""
     Generate a Discord timestamp.
 
     If TIME is omitted, uses the current time.
 
-    TIME should be of the form \\<date>,\\<time>.
-    Either \\<date> or \\<time> can be omitted, in which case you should omit the
+    TIME should be of the form \<date>,\<time>.
+    Either \<date> or \<time> can be omitted, in which case you should omit the
     comma as well.
 
-    \\<date> should be of the following form: ddmmmyyyy.
+    \<date> should be of the following form: ddmmmyyyy.
 
     dd and yyyy should be numbers.
 
     mmm should be at least the first three letters of the month. For instance,
     for August you can specify aug, augu, augus, or august.
 
-    You can omit the year to specify the current year. You can omit \\<date>
+    You can omit the year to specify the current year. You can omit \<date>
     entirely to specify the current date.
 
     The command will also accept the following special values:
@@ -69,13 +69,13 @@ def get_timestamp(
     - tomorrow
     - yesterday
 
-    \\<time> should be of the following form: hhmmss(am/pm).
+    \<time> should be of the following form: hhmmss(am/pm).
 
     You can omit seconds to specify 0. If you do, you can omit minutes to specify
-    0 as well. If you omit (am/pm), 24-hour time will be used. You can omit \\<time>
+    0 as well. If you omit (am/pm), 24-hour time will be used. You can omit \<time>
     entirely to specify midnight.
 
-    If you omit both \\<date> and \\<time>, the current date and time will be used.
+    If you omit both \<date> and \<time>, the current date and time will be used.
 
     The command will also accept the following special values:
     now (the current time, but not necessarily the current date),
