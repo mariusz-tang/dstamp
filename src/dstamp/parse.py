96f94650f61121c0b6d029124cff8de71f66d4ad
Mariusz Tang <dev@mariusztang.com>
2026-05-19 17:36:05 +0100

    feat(parse): add time parser

--- src/dstamp/parse.py
+++ src/dstamp/parse.py
@@ -24,6 +24,10 @@ class InvalidDateError(ParserInputError):
     """Raised when an invalid date object would be created."""
 
 
+class InvalidTimeError(ParserInputError):
+    """Raised when an invalid time object would be created."""
+
+
 MONTHS = [
     "jan",
     "feb",
@@ -54,3 +58,29 @@ def date(input: str) -> dt.date:
         return dt.date(year, month, day)
     except ValueError as e:
         raise InvalidDateError(input) from e
+
+
+def time(input: str) -> dt.time:
+    """Parse `input` as a time."""
+    m = re.fullmatch(r"(\d{1,2})(\d{2})?(\d{2})?(am|pm)?", input.lower())
+    if not m:
+        raise FormatError(input)
+
+    hour = int(m[1])
+    if ampm := m[4]:
+        if not 1 <= hour <= 12:
+            # Cannot use 24 hour time and ampm simultaneously.
+            raise FormatError(input)
+        if ampm == "pm" and hour != 12:
+            hour += 12
+        elif ampm == "am" and hour == 12:
+            # 12am is the 0th hour.
+            hour = 0
+
+    minute = int(m[2]) if m[2] else 0
+    second = int(m[3]) if m[3] else 0
+
+    try:
+        return dt.time(hour, minute, second)
+    except ValueError as e:
+        raise InvalidTimeError(input) from e
--- tests/test_parse.py
+++ tests/test_parse.py
@@ -1,4 +1,4 @@
-from datetime import date
+from datetime import date, time
 
 import freezegun
 import pytest
@@ -65,3 +65,85 @@ def test_date_checks_full_match() -> None:
 
 def test_date_ignores_case() -> None:
     assert parse.date("10AuG") == parse.date("10aug")
+
+
+@pytest.mark.parametrize("input", range(24))
+def test_time_24_hour_hour_only(input: int) -> None:
+    assert parse.time(str(input)) == time(input)
+
+
+@pytest.mark.parametrize(
+    ("input", "hour"),
+    [
+        ("12pm", 12),
+        ("12am", 0),
+        ("3am", 3),
+        ("6pm", 18),
+    ],
+)
+def test_time_12_hour_hour_only(input: str, hour: int) -> None:
+    assert parse.time(input) == time(hour)
+
+
+@pytest.mark.parametrize(
+    ("input", "hour", "minute"),
+    [
+        ("1104", 11, 4),
+        ("304", 3, 4),
+        ("1754", 17, 54),
+        ("006", 0, 6),
+        ("0006", 0, 6),
+        ("2359", 23, 59),
+        ("1104am", 11, 4),
+        ("304pm", 15, 4),
+        ("1254am", 0, 54),
+        ("1259pm", 12, 59),
+    ],
+)
+def test_time_hour_and_minute_only(input: str, hour: int, minute: int) -> None:
+    assert parse.time(input) == time(hour, minute)
+
+
+@pytest.mark.parametrize(
+    ("input", "hour", "minute", "second"),
+    [
+        ("00000", 0, 0, 0),
+        ("000000", 0, 0, 0),
+        ("23421", 2, 34, 21),
+        ("172822", 17, 28, 22),
+        ("111111pm", 23, 11, 11),
+        ("14932am", 1, 49, 32),
+    ],
+)
+def test_time_full(input: str, hour: int, minute: int, second: int) -> None:
+    assert parse.time(input) == time(hour, minute, second)
+
+
+def test_time_ignores_case() -> None:
+    assert parse.time("10aM") == parse.time("10am")
+
+
+@pytest.mark.parametrize("input", ["2500", "360", "31560"])
+def test_time_invalid_time_raises(input: str) -> None:
+    with pytest.raises(parse.InvalidTimeError) as e:
+        parse.time(input)
+    assert e.value.input == input
+
+
+def test_time_invalid_format_raises() -> None:
+    with pytest.raises(parse.FormatError) as e:
+        parse.time("not a time")
+    assert e.value.input == "not a time"
+
+
+def test_time_checks_full_match() -> None:
+    with pytest.raises(parse.FormatError) as e:
+        parse.time("12pm plus some extra")
+    assert e.value.input == "12pm plus some extra"
+
+
+@pytest.mark.parametrize("input", ["0pm", "13am", "16pm"])
+def test_time_24_hour_with_ampm_is_a_format_error(input: str) -> None:
+    with pytest.raises(parse.FormatError) as e:
+        parse.time(input)
+    assert e.value.input == input
