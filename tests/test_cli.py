c926dcfea03120d1bb38ade9a46617b430476d93
Mariusz Tang <dev@mariusztang.com>
2026-05-22 00:01:37 +0100

    refactor(tests): add GetRunner type alias
    This makes test signatures shorter and makes them easier to type.

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -46,8 +46,11 @@ class GetOutput:
         self.copied_to_clipboard = "Copied to clipboard!" in raw_output
 
 
+type GetRunner = AppRunner[GetOutput]
+
+
 @pytest.fixture
-def get(app: AppRunner[str]) -> AppRunner[GetOutput]:
+def get(app: AppRunner[str]) -> GetRunner:
     def run(*args: str) -> GetOutput:
         return GetOutput(app("get", *args))
 
@@ -68,33 +71,33 @@ def test_no_args_prints_help(
 
 
 @freezegun.freeze_time(datetime.fromtimestamp(1234567890.2139))
-def test_get_no_args_returns_current_time(get: AppRunner[GetOutput]) -> None:
+def test_get_no_args_returns_current_time(get: GetRunner) -> None:
     output = get()
     assert output.timestamp == 1234567890
 
 
-def test_get_no_args_uses_long_full_format(get: AppRunner[GetOutput]) -> None:
+def test_get_no_args_uses_long_full_format(get: GetRunner) -> None:
     output = get()
     assert output.format_code == "F"
 
 
-def test_get_date_only_uses_midnight(get: AppRunner[GetOutput]) -> None:
+def test_get_date_only_uses_midnight(get: GetRunner) -> None:
     output = get("10jan2025")
     assert output.timestamp == datetime(2025, 1, 10).timestamp()
 
 
 @freezegun.freeze_time("October 10 2025")
-def test_get_time_only_uses_current_date(get: AppRunner[GetOutput]) -> None:
+def test_get_time_only_uses_current_date(get: GetRunner) -> None:
     output = get("9pm")
     assert output.timestamp == datetime(2025, 10, 10, 21).timestamp()
 
 
-def test_get_date_and_time(get: AppRunner[GetOutput]) -> None:
+def test_get_date_and_time(get: GetRunner) -> None:
     output = get("25jun2028", "550pm")
     assert output.timestamp == datetime(2028, 6, 25, 17, 50).timestamp()
 
 
-def test_get_no_args_produces_no_error_text(get: AppRunner[GetOutput]) -> None:
+def test_get_no_args_produces_no_error_text(get: GetRunner) -> None:
     output = get()
     assert not output.error_text
 
@@ -112,14 +115,14 @@ def test_get_no_args_produces_no_error_text(get: AppRunner[GetOutput]) -> None:
     ],
 )
 def test_get_invalid_data_prints_error(
-    get: AppRunner[GetOutput], args: list[str], expected_error_text: str
+    get: GetRunner, args: list[str], expected_error_text: str
 ) -> None:
     output = get(*args)
     assert expected_error_text in output.error_text
 
 
 def test_get_copy_enabled_by_default(
-    get: AppRunner[GetOutput], copy_mock: unittest.mock.Mock
+    get: GetRunner, copy_mock: unittest.mock.Mock
 ) -> None:
     output = get()
     timestamp = f"<t:{output.timestamp}:{output.format_code}>"
@@ -127,27 +130,21 @@ def test_get_copy_enabled_by_default(
     assert output.copied_to_clipboard
 
 
-def test_get_copy_option(
-    get: AppRunner[GetOutput], copy_mock: unittest.mock.Mock
-) -> None:
+def test_get_copy_option(get: GetRunner, copy_mock: unittest.mock.Mock) -> None:
     output = get("--copy")
     timestamp = f"<t:{output.timestamp}:{output.format_code}>"
     copy_mock.assert_called_with(timestamp)
     assert output.copied_to_clipboard
 
 
-def test_get_copy_short_option(
-    get: AppRunner[GetOutput], copy_mock: unittest.mock.Mock
-) -> None:
+def test_get_copy_short_option(get: GetRunner, copy_mock: unittest.mock.Mock) -> None:
     output = get("-c")
     timestamp = f"<t:{output.timestamp}:{output.format_code}>"
     copy_mock.assert_called_with(timestamp)
     assert output.copied_to_clipboard
 
 
-def test_get_no_copy_option(
-    get: AppRunner[GetOutput], copy_mock: unittest.mock.Mock
-) -> None:
+def test_get_no_copy_option(get: GetRunner, copy_mock: unittest.mock.Mock) -> None:
     output = get("--no-copy")
     copy_mock.assert_not_called()
     assert not output.copied_to_clipboard
@@ -167,7 +164,7 @@ def test_get_no_copy_option(
     ],
 )
 def test_get_output_format_option(
-    get: AppRunner[GetOutput],
+    get: GetRunner,
     option_name: str,
     option_value: str,
     expected_format_code: str,
