081d1ddd7a33ce53695c59afb0fefe84e0fc0786
Mariusz Tang <dev@mariusztang.com>
2026-06-04 00:23:09 +0100

    tests: clean up config path fixture
    - Add documentation.
    - Use a more specific patch target.

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,5 +8,6 @@ import pytest_mock
 def config_path(
     tmp_path: pathlib.Path, mocker: pytest_mock.MockerFixture
 ) -> pathlib.Path:
-    mocker.patch("platformdirs.user_config_path").return_value = tmp_path
+    """Use a temporary directory for the config path during tests."""
+    mocker.patch("dstamp.config.platformdirs.user_config_path").return_value = tmp_path
     return tmp_path / "config.toml"
